b45d6ba402c532e54a41da9caf794ac5de217fd3
L-Grancher <loic.grancher@scholarfab.school>
2024-02-07 16:06:38 +0100

    Implémenter page formateur/tuteur/apprenant

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -22,13 +22,13 @@ class MainController extends AbstractController
     #[Route('/homeFormateur', name: 'homeFormateur')]
     public function homeFormateur(): Response
     {
+        //Recupérer les matieres du formateur
         $user = $this->getUser();
         $matieres = $user->getMatiereEnseignee();
+        // Récupérer les formations de chaque matiere
         foreach ($matieres as $matiere) {
             $formations = $matiere->getFormation();
-        } 
-
-
+        }
 
         return $this->render('main/homeFormateur.html.twig', [
             'matieres' => $matieres,
@@ -40,27 +40,47 @@ class MainController extends AbstractController
     #[Route('/homeTuteur', name: 'homeTuteur')]
     public function homeTuteur(): Response
     {
-        return $this->render('main/homeTuteur.html.twig');
+        //Recupérer les apprenants du tuteur
+        $user = $this->getUser();
+        $apprenants = $user->getApprenants();
+        //Récupérer la formation de chaque apprenant
+        foreach ($apprenants as $apprenant) {
+            $formation = $apprenant->getFormationSuivie();
+        }
+        // Récupérer les matières de cette formation
+        $matieres = $formation->getMatieres();
+
+        return $this->render('main/homeTuteur.html.twig', [
+            'apprenants' => $apprenants,
+            'matieres' => $matieres,
+            'formation' => $formation
+        ]);
     }
 
     #[IsGranted("ROLE_APPRENANT")]
     #[Route('/homeApprenant', name: 'homeApprenant')]
     public function homeApprenant(): Response
     {
-    
-        return $this->render('main/homeApprenant.html.twig');
+        $user = $this->getUser();
+        $formation = $user->getFormationSuivie();
+        $matieres = $formation->getMatieres();
+
+        return $this->render('main/homeApprenant.html.twig', [
+            'formation' => $formation,
+            'matieres' => $matieres
+        ]);
     }
 
     #[Route('/homeAll', name: 'homeAll')]
     public function homeAll(): Response
     {
-        if( $this->isGranted('ROLE_FORMATEUR') ){
+        if ($this->isGranted('ROLE_FORMATEUR')) {
             return $this->redirectToRoute('homeFormateur');
         }
-        if( $this->isGranted('ROLE_TUTEUR') ){
+        if ($this->isGranted('ROLE_TUTEUR')) {
             return $this->redirectToRoute('homeTuteur');
         }
-        if( $this->isGranted('ROLE_APPRENANT') ){
+        if ($this->isGranted('ROLE_APPRENANT')) {
             return $this->redirectToRoute('homeApprenant');
         }
         return $this->redirectToRoute('error');
