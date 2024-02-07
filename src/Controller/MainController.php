b3ec84d0ba99189d4d157b43a7dd827c41989c32
L-Grancher <loic.grancher@scholarfab.school>
2024-02-07 10:10:16 +0100

    Added a few routes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,12 +5,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+
 
 class MainController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    public function index(): RedirectResponse
     {
-        return $this->render('main/index.html.twig');
+        return $this->redirectToRoute('app_login');
+    }
+
+    // #[IsGranted("ROLE_ADMIN")]
+    #[Route('/homeFormateur', name: 'homeFormateur')]
+    public function homeFormateur(): Response
+    {
+        return $this->render('main/homeFormateur.html.twig');
+    }
+
+    // #[IsGranted("ROLE_ADMIN")]
+    #[Route('/homeTuteur', name: 'homeTuteur')]
+    public function homeTuteur(): Response
+    {
+        return $this->render('main/homeTuteur.html.twig');
+    }
+
+    // #[IsGranted("ROLE_ADMIN")]
+    #[Route('/homeApprenant', name: 'homeApprenant')]
+    public function homeApprenant(): Response
+    {
+        return $this->render('main/homeApprenant.html.twig');
     }
 }
