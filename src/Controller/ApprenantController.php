1118d3d051bb17cbe18074b0098a05860a542802
L-Grancher <loic.grancher@scholarfab.school>
2024-02-16 10:17:58 +0100

    Backend tuteur & apprenant finished

--- src/Controller/ApprenantController.php
+++ src/Controller/ApprenantController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Note;
 use App\Entity\Matiere;
 use App\Entity\Formation;
 use App\Entity\Utilisateur;
@@ -14,10 +15,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 
+
 class ApprenantController extends AbstractController
 {
+    //ACCUEIL APPRENANT (liste les matieres avec note/programme pour chaque)
     #[IsGranted("ROLE_APPRENANT")]
-#[Route('/Apprenant/apprenantHome', name: 'apprenantHome')]
+    #[Route('/Apprenant/apprenantHome', name: 'apprenantHome')]
     public function ApprenantHome(): Response
     {
         $user = $this->getUser();
@@ -26,10 +29,12 @@ class ApprenantController extends AbstractController
 
         return $this->render('main/apprenant/apprenantHome.html.twig', [
             'formation' => $formation,
-            'matieres' => $matieres
+            'matieres' => $matieres,
+            'user' => $user
         ]);
     }
 
+    //PROGRAMME DE LA MATIERE
     #[IsGranted("ROLE_APPRENANT")]
     #[Route('/Apprenant/programme/{matiereId}', name: 'apprenantProgramme')]
     public function ApprenantProgramme($matiereId, EntityManagerInterface $entityManager): Response
@@ -46,19 +51,19 @@ class ApprenantController extends AbstractController
         ]);
     }
 
+    //NOTES DE L'APPRENANT
     #[IsGranted("ROLE_APPRENANT")]
-    #[Route('/Apprenant/notes/{matiereId}', name: 'apprenantNotes')]
-    public function ApprenantNotes($matiereId, EntityManagerInterface $entityManager): Response
+    #[Route('/Apprenant/notes', name: 'apprenantNotes')]
+    public function ApprenantNotes(EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
-        //Recupérer le repo de la matière selectionnée {id}
-        $matiereRepo = $entityManager->getRepository(Matiere::class);
-        //extraire l'ID et les notes de la matière
-        $matiere = $matiereRepo->find($matiereId);
+        //Recupérer le repo des notes de l'utilisateur
+        $repoNotes = $entityManager->getRepository(Note::class);
+        $notes = $repoNotes->findBy(['apprenant' => $user]);
 
         return $this->render('main/apprenant/apprenantNotes.html.twig', [
-            'matiere' => $matiere,
-            'user' => $user
+            'user' => $user,
+            'notes' => $notes
         ]);
     }
 }
--- src/Controller/TuteurController.php
+++ src/Controller/TuteurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Note;
 use App\Entity\Matiere;
 use App\Entity\Formation;
 use App\Entity\Utilisateur;
@@ -16,6 +17,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TuteurController extends AbstractController
 {
+    //ACCUEIL TUTEUR (liste les apprenants)
     #[IsGranted("ROLE_TUTEUR")]
     #[Route('/Tuteur/tuteurHome', name: 'tuteurHome')]
     public function TuteurHome(): Response
@@ -25,10 +27,12 @@ class TuteurController extends AbstractController
         $apprenants = $user->getApprenants();
 
         return $this->render('main/tuteur/tuteurHome.html.twig', [
-            'apprenants' => $apprenants
+            'apprenants' => $apprenants,
+            'user' => $user
         ]);
     }
 
+    //PROFIL APPRENANT (liste les matieres)
     #[IsGranted("ROLE_TUTEUR")]
     #[Route('/Tuteur/{apprenantId}/', name: 'tuteurApprenant')]
     public function TuteurApprenant($apprenantId, EntityManagerInterface $entityManager): Response
@@ -40,57 +44,47 @@ class TuteurController extends AbstractController
         $formation = $apprenant->getFormationSuivie();
         $matieres = $formation->getMatieres();
 
-        return $this->render('main/tuteur/tuteurMatiere.html.twig', [
+    return $this->render('main/tuteur/tuteurApprenant.html.twig', [
             'apprenant' => $apprenant,
             'matieres' => $matieres
         ]);
     }
 
+    //FICHE MATIERE (liste le programme)
     #[IsGranted("ROLE_TUTEUR")]
     #[Route('/Tuteur/{apprenantId}/Matiere/{matiereId}', name: 'tuteurMatiere')]
     public function TuteurMatiere($apprenantId, $matiereId, EntityManagerInterface $entityManager): Response
     {
+        //Recupérer le repo de l'apprenant {id}
+        $apprenantRepo = $entityManager->getRepository(Utilisateur::class);
+        $apprenant = $apprenantRepo->find($apprenantId);
         //Recupérer le repo de la matière selectionnée {id}
         $matiereRepo = $entityManager->getRepository(Matiere::class);
-        //extraire l'ID et les notes de la matière
         $matiere = $matiereRepo->find($matiereId);
 
         return $this->render('main/tuteur/tuteurMatiere.html.twig', [
-            'matiere' => $matiere
+            'matiere' => $matiere,
+            'apprenant' => $apprenant
         ]);
     }
 
+    //FICHE NOTES (liste les notes)
     #[IsGranted("ROLE_TUTEUR")]
-    #[Route('/Tuteur/{apprenantId}/Notes/{matiereId}', name: 'tuteurNotes')]
-    public function TuteurNotes($matiereId, $apprenantId, EntityManagerInterface $entityManager): Response
+    #[Route('/Tuteur/{apprenantId}/Notes', name: 'tuteurNotes')]
+    public function TuteurNotes($apprenantId, EntityManagerInterface $entityManager): Response
     {
-        //Recupérer le repo de la matière selectionnée {id}
-        $matiereRepo = $entityManager->getRepository(Matiere::class);
-        //extraire l'ID et les notes de la matière
-        $matiere = $matiereRepo->find($matiereId);
-
+        //Recupérer les infos de l'apprenant
         $apprenantRepo = $entityManager->getRepository(Utilisateur::class);
-        //extraire l'ID et les apprenants de l'apprenant
         $apprenant = $apprenantRepo->find($apprenantId);
+        //Recupérer les notes de l'apprenant
+        $notesRepo = $entityManager->getRepository(Note::class);
+        $notes = $notesRepo->findBy(['apprenant' => $apprenant]);
+
+        // Do something with the notes...
 
         return $this->render('main/tuteur/tuteurNotes.html.twig', [
-            'matiere' => $matiere
-            ,'apprenant' => $apprenant
-            
-        ]);
-    }
-
-    #[IsGranted("ROLE_TUTEUR")]
-    #[Route('/Tuteur/{apprenantId}/Programme/{matiereId}', name: 'tuteurProgramme')]
-    public function TuteurProgramme($matiereId,EntityManagerInterface $entityManager): Response
-    {
-        //Recupérer le repo de la matière selectionnée {id}
-        $matiereRepo = $entityManager->getRepository(Matiere::class);
-        //extraire l'ID et les notes de la matière
-        $matiere = $matiereRepo->find($matiereId);
-
-        return $this->render('main/tuteur/tuteurProgramme.html.twig', [
-            'matiere' => $matiere
+            'apprenant' => $apprenant,
+            'notes' => $notes
         ]);
     }
 }
