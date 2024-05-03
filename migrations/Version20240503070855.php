2983031d7db9b2caf5dac446f4f4d7f17acd64f6
L-Grancher <loic.grancher@scholarfab.school>
2024-05-03 15:13:05 +0200

    symfony responsive 90%

--- migrations/Version20240503070855.php
+++ migrations/Version20240503070855.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240216153409 extends AbstractMigration
+final class Version20240503070855 extends AbstractMigration
 {
     public function getDescription(): string
     {
